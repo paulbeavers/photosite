01236dc5a50bcb5d6937b4a9d6fc8717dcbd2861
paul <paul_beavers@mac.com>
2021-04-09 22:49:06 -0500

    Adding new files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Products from './Products';
+import Rating from './Rating';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	formatName(user) {
+		return user.firstName + user.lastName;
+	}
+
+
+
+	render() {
+		const isValid=false;
+		const user = {
+			firstName: 'Paul',
+			lastName: 'Beavers'
+		};
+
+		return(
+			<div className="App">
+				<Products />
+			</div>
+		);
+	}
 }
 
 export default App;
